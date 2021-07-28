13d981c72912a28156e25d563bb6c0f9dc644d20
Mari Taksa <welch.mari@gmail.com>
2021-07-28 10:03:14 -0500

    it ain't pretty but it works

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant: { id, name, image, price } }) {
+  const [inStock, setInStock] = useState(true)
+
+  function toggleInStock() {
+    setInStock((inStock) => !inStock)
+  }
+  
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: ${price}</p>
+        <button className="primary" onClick={toggleInStock}>{inStock ? "In Stock" : "Sold Out"}</button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(plants => setPlants(plants))
+  }, [])
+
+  function handleNewPlant(newPlant) {
+    const updatedList = [...plants, newPlant]
+    setPlants(updatedList)
+  }
+
+  const newPlantArray = plants.filter((plant) => {
+    return plant.name.includes(searchInput)
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleNewPlant={handleNewPlant}/>
+      <Search searchInput={searchInput} setSearchInput={setSearchInput}/>
+      <PlantList plants={newPlantArray}/>
     </main>
   );
 }
